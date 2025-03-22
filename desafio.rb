19d66d6a4f922bcbc339bb7b0fa8c25ae6e23590
cremubi <allanfernandospereira@gmail.com>
2025-03-22 15:43:51 -0300

    entregar projeto

--- desafio.rb
+++ desafio.rb
@@ -1,8 +1,8 @@
-print("Olá! Seja bem-vindo à Biblioteca! Qual é o seu nome completo? ")
+puts "Olá! Seja bem-vindo à Biblioteca! Qual é o seu nome completo? "
 fullName = gets.chomp
 firstName = fullName.split.first
 
-print("É um prazer te conhecer, #{firstName}! Para fazer o seu cadastro, também precisamos da sua idade. Quantos anos você tem? ")
+puts "É um prazer te conhecer, #{firstName}! Para fazer o seu cadastro, também precisamos da sua idade. Quantos anos você tem? "
 age = gets.chomp.to_i
 
-print("Cadastro realizado com sucesso! Nome completo: #{fullName}, Idade: #{age} anos.")
+puts "Cadastro realizado com sucesso! Nome completo: #{fullName}, Idade: #{age} anos."
